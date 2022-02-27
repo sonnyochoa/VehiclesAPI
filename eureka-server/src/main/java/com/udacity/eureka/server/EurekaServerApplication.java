f289cd86c37ea53813fde0caf2e569be2dc12ab2
sonnyochoa <0xsonny@gmail.com>
2022-02-27 12:31:14 -0800

    Built a basic Eureka Server.

---
+++ eureka-server/src/main/java/com/udacity/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,9 @@
+package com.udacity.eureka.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+}
