f39499a9161dcc9d1d97215fec1939cb563609ae
Sonny Ochoa <0xsonny@gmail.com>
2022-11-22 12:23:07 -0800

    Converted the Pricing Service API to a microservice using SPring Data REST.

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,11 +1,17 @@
 package com.udacity.pricing.domain.price;
 
+
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+//@Entity
 public class Price {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
 
     private String currency;
     private BigDecimal price;
@@ -19,6 +25,13 @@ public class Price {
         this.price = price;
         this.vehicleId = vehicleId;
     }
+
+//    public Price(Long id, String currency, BigDecimal price, Long vehicleId) {
+//        this.id = id;
+//        this.currency = currency;
+//        this.price = price;
+//        this.vehicleId = vehicleId;
+//    }
 
     public String getCurrency() {
         return currency;
